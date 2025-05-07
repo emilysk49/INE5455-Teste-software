465aaf85c091eb1487fe924e55ab6951fe373481
emilysk49 <emilytti@me.com>
2025-05-07 10:50:28 -0300

    teste11-código de implementação

--- src/funcionario.py
+++ src/funcionario.py
@@ -2,8 +2,12 @@ class Funcionario():
     def __init__(self, nome):
         self.nome = nome
         self.projetos = []
+        self.salario = None
 
     def adicionar_projeto(self, projeto):
         if projeto in self.projetos:
             raise ValueError("Projeto já adicionado.")
         self.projetos.append(projeto)
+
+    def adicionar_salario(self, salario):
+        self.salario = salario
