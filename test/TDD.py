4dcdb3019ad43a0c52353ed496614632c730db6c
emilysk49 <emilytti@me.com>
2025-05-07 10:00:28 -0300

    test2-código de teste

--- test/TDD.py
+++ test/TDD.py
@@ -7,6 +7,10 @@ class TDD(unittest.TestCase):
         self.assertIsInstance(empresa_W, Empresa)
         self.assertEqual(empresa_W.nome, "W")
 
+    def test2_criar_funcionario(self):
+        jose = Funcionario("José")
+        self.assertIsInstance(jose, Funcionario)
+        self.assertEqual(jose.nome, "Jose")
 
 if __name__ == '__main__':
     unittest.main()
