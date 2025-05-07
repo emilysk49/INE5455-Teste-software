a5cb011afb96a6ae40eb6d72d13305a47b53a89d
emilysk49 <emilytti@me.com>
2025-05-07 10:19:26 -0300

    teste6-código de implementação

--- src/empresa.py
+++ src/empresa.py
@@ -4,8 +4,12 @@ class Empresa():
     def __init__(self, nome):
         self.nome = nome
         self.funcionarios = []
+        self.projetos = []
 
     def adicionar_funcionario(self, funcionario):
         if (funcionario in self.funcionarios):
             raise ValueError("Funcionário já cadastrado.")
         self.funcionarios.append(funcionario)
+
+    def adicionar_projeto(self, projeto):
+        self.projetos.append(projeto)
