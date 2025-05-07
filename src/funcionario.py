0797ed87abf1da73f282597f20d84558b5af325f
emilysk49 <emilytti@me.com>
2025-05-07 10:47:55 -0300

    teste10.2-código de implementação

--- src/funcionario.py
+++ src/funcionario.py
@@ -4,4 +4,6 @@ class Funcionario():
         self.projetos = []
 
     def adicionar_projeto(self, projeto):
+        if projeto in self.projetos:
+            raise ValueError("Projeto já adicionado.")
         self.projetos.append(projeto)
