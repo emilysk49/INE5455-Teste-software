205f20268c56766b02b3c2cb36cc1edff320bc4f
emilysk49 <emilytti@me.com>
2025-05-07 10:12:47 -0300

    teste4-código de implementação

--- src/empresa.py
+++ src/empresa.py
@@ -1,3 +1,9 @@
+from src.funcionario import Funcionario
+
 class Empresa():
     def __init__(self, nome):
         self.nome = nome
+        self.funcionarios = []
+
+    def adicionar_funcionario(self, funcionario):
+        self.funcionarios.append(funcionario)
